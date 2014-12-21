378ab7d18589f19fb8934f8f7580d379e7715ae7
perlancar <perlancar@gmail.com>
2014-12-21 16:03:55 +0000

    Import of PERLANCAR/Getopt-Long-Util-0.79.tar.gz from CPAN.
    gitpan-cpan-distribution: Getopt-Long-Util
    gitpan-cpan-version:      0.79
    gitpan-cpan-path:         PERLANCAR/Getopt-Long-Util-0.79.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/Getopt/Long/Util.pm
+++ lib/Getopt/Long/Util.pm
@@ -1,19 +1,22 @@
 package Getopt::Long::Util;
 
+our $DATE = '2014-12-21'; # DATE
+our $VERSION = '0.79'; # VERSION
+
 use 5.010001;
 use strict;
 use warnings;
 use experimental 'smartmatch';
-
-our $DATE = '2014-12-09'; # DATE
-our $VERSION = '0.78'; # VERSION
 
 require Exporter;
 our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw(
                        parse_getopt_long_opt_spec
                        humanize_getopt_long_opt_spec
+                       detect_getopt_long_script
                );
+
+our %SPEC;
 
 sub parse_getopt_long_opt_spec {
     my $optspec = shift;
@@ -101,7 +104,114 @@ sub humanize_getopt_long_opt_spec {
     $res;
 }
 
-#ABSTRACT: Utilities for Getopt::Long
+$SPEC{detect_getopt_long_script} = {
+    v => 1.1,
+    summary => 'Detect whether a file is a Getopt::Long-based CLI script',
+    description => <<'_',
+
+The criteria are:
+
+* the file must exist and readable;
+
+* (optional, if `include_noexec` is false) file must have its executable mode
+  bit set;
+
+* content must start with a shebang C<#!>;
+
+* either: must be perl script (shebang line contains 'perl') and must contain
+  something like `use Getopt::Long`;
+
+_
+    args => {
+        filename => {
+            summary => 'Path to file to be checked',
+            schema => 'str*',
+            description => <<'_',
+
+Either `filename` or `string` must be specified.
+
+_
+        },
+        string => {
+            summary => 'Path to file to be checked',
+            schema => 'buf*',
+            description => <<'_',
+
+Either `file` or `string` must be specified.
+
+_
+        },
+        include_noexec => {
+            summary => 'Include scripts that do not have +x mode bit set',
+            schema  => 'bool*',
+            default => 1,
+        },
+    },
+};
+sub detect_getopt_long_script {
+    my %args = @_;
+
+    (defined($args{filename}) xor defined($args{string}))
+        or return [400, "Please specify either filename or string"];
+    my $include_noexec  = $args{include_noexec}  // 1;
+
+    my $yesno = 0;
+    my $reason = "";
+
+    my $str = $args{string};
+  DETECT:
+    {
+        if (defined $args{filename}) {
+            my $fn = $args{filename};
+            unless (-f $fn) {
+                $reason = "'$fn' is not a file";
+                last;
+            };
+            if (!$include_noexec && !(-x _)) {
+                $reason = "'$fn' is not an executable";
+                last;
+            }
+            my $fh;
+            unless (open $fh, "<", $fn) {
+                $reason = "Can't be read";
+                last;
+            }
+            # for efficiency, we read a bit only here
+            read $fh, $str, 2;
+            unless ($str eq '#!') {
+                $reason = "Does not start with a shebang (#!) sequence";
+                last;
+            }
+            my $shebang = <$fh>;
+            unless ($shebang =~ /perl/) {
+                $reason = "Does not have 'perl' in the shebang line";
+                last;
+            }
+            seek $fh, 0, 0;
+            {
+                local $/;
+                $str = <$fh>;
+            }
+        }
+        unless ($str =~ /\A#!/) {
+            $reason = "Does not start with a shebang (#!) sequence";
+            last;
+        }
+        unless ($str =~ /\A#!.*perl/) {
+            $reason = "Does not have 'perl' in the shebang line";
+            last;
+        }
+        if ($str =~ /^\s*(use|require)\s+Getopt::Long(\s|;)/m) {
+            $yesno = 1;
+            last DETECT;
+        }
+        $reason = "Can't find any statement requiring Getopt::Long module";
+    } # DETECT
+
+    [200, "OK", $yesno, {"func.reason"=>$reason}];
+}
+
+# ABSTRACT: Utilities for Getopt::Long
 
 __END__
 
@@ -115,7 +225,7 @@ Getopt::Long::Util - Utilities for Getopt::Long
 
 =head1 VERSION
 
-This document describes version 0.78 of Getopt::Long::Util (from Perl distribution Getopt-Long-Util), released on 2014-12-09.
+This document describes version 0.79 of Getopt::Long::Util (from Perl distribution Getopt-Long-Util), released on 2014-12-21.
 
 =head1 FUNCTIONS
 
@@ -141,6 +251,64 @@ Convert L<Getopt::Long> option specification like C<help|h|?> or <--foo=s> or
 C<debug!> into, respectively, C<--help, -h, -?> or C<--foo=s> or C<--(no)debug>.
 Will die if can't parse C<$str>. The output is suitable for including in
 help/usage text.
+
+=head2 detect_getopt_long_script(%args) => envres
+
+
+=head2 detect_getopt_long_script(%args) -> [status, msg, result, meta]
+
+Detect whether a file is a Getopt::Long-based CLI script.
+
+The criteria are:
+
+=over
+
+=item * the file must exist and readable;
+
+=item * (optional, if C<include_noexec> is false) file must have its executable mode
+bit set;
+
+=item * content must start with a shebang C<#!>;
+
+=item * either: must be perl script (shebang line contains 'perl') and must contain
+something like C<use Getopt::Long>;
+
+=back
+
+Arguments ('*' denotes required arguments):
+
+=over 4
+
+=item * B<filename> => I<str>
+
+Path to file to be checked.
+
+Either C<filename> or C<string> must be specified.
+
+=item * B<include_noexec> => I<bool> (default: 1)
+
+Include scripts that do not have +x mode bit set.
+
+=item * B<string> => I<buf>
+
+Path to file to be checked.
+
+Either C<file> or C<string> must be specified.
+
+=back
+
+Return value:
+
+Returns an enveloped result (an array).
+
+First element (status) is an integer containing HTTP status code
+(200 means OK, 4xx caller error, 5xx function error). Second element
+(msg) is a string containing error message, or 'OK' if status is
+200. Third element (result) is optional, the actual result. Fourth
+element (meta) is called result metadata and is optional, a hash
+that contains extra information.
+
+ (any)
 
 =head1 SEE ALSO
 
